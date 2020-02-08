1c5473f2268f64647db2b4237d9495d2ea92d4f2
nimish <nimishsantosh107@icloud.com>
2020-02-08 14:17:12 +0530

    peer connections

--- app/client/script.js
+++ app/client/script.js
@@ -1,1 +1,28 @@
 var peer = new Peer();
+
+//INITIATOR USES SENDCONN, RESPONDER USES RECVCONN
+var sendconn = null;
+var recvconn = null;
+
+function connectPeer(peerid) {
+    sendconn = peer.connect(peerid);
+    sendconn.on('open', function () {
+        console.log(`CONNECTED TO ${peerid}`);
+        sendconn.send(`CONNECTED TO ${peer.id}`);
+    });
+
+    sendconn.on('data', function (data) {
+        console.log(data);
+    });
+}
+
+peer.on('connection', function (conn) {
+    recvconn = conn;
+    recvconn.on('data', function (data) {
+        console.log(data);
+    });
+});
+
+seedButton.addEventListener('click', function (event) {
+    console.log(seedInput.files);
+});
